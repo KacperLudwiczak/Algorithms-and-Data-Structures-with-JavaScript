72fbde88ea296083c443cb1c76d95392769de8e4
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2025-02-16 12:27:00 +0100

    Add Big O Notation

---
+++ Big O Notation/add_up_to_slower.js
@@ -0,0 +1,15 @@
+/*
+We want to write a function that calculates the sum of all numbers from 1 up to (and including) some number n.
+*/ 
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
