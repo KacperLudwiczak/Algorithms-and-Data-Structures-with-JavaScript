250ef900725a3f8b7cd2087011fcf9ab2ed65400
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2025-02-19 16:03:53 +0100

    Add Data Structures and Classes

---
+++ Data Structures and Classes/class_keyword.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor(firstName, lastName, year){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = year;
+    }
+}
+
+let firstStudent = new Student("Kacper", "Lud",1);
+let secondStudent = new Student("Justi", "Koz",2);
+
+
